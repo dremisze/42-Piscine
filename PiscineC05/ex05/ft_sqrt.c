b0688aae9141cdd0e67aa3820e7e7cd3570511e6
Dariusz Remiszewski <dremisze@c3r2s3.42warsaw.pl>
2023-12-07 21:55:38 +0100

    a

--- PiscineC05/ex05/ft_sqrt.c
+++ PiscineC05/ex05/ft_sqrt.c
@@ -6,46 +6,44 @@
 /*   By: dremisze <dremisze@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/06 13:19:40 by dremisze          #+#    #+#             */
-/*   Updated: 2023/12/06 19:52:49 by dremisze         ###   ########.fr       */
+/*   Updated: 2023/12/07 18:05:44 by dremisze         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_sqrt(int nb)
 {
-	int	a;
-	int	b;
-	int	middle;
+	int	i;
 
-	a = 0;
-	b = nb;
-	if (nb < 0)
-		return (0);
-	if (nb == 0 || nb == 1)
-		return (nb);
-	while (a <= b)
+	i = 1;
+	if (nb > 0)
 	{
-		middle = a + (b - a) / 2;
-		if (middle * middle == nb)
-			return (middle);
-		if (middle * middle < nb)
-			a = middle + 1;
-		else
-			b = middle - 1;
+		while (i * i <= nb)
+		{
+			if (i * i == nb)
+				return (i);
+			if (i > 46340)
+				return (0);
+			i++;
+		}
 	}
 	return (0);
 }
 /*
 #include <stdio.h>
+#include <stdlib.h>
 
-int main()
+int main(int argc, char *argv[])
 {
-	int i;
-	int result;
-
-	i = 4;
-	result = ft_sqrt(i);
-	printf("Pierwiastek z %d wynosi %d", i, result);
-	return (0);
+	if (argc == 2)
+	{
+		int number = atoi(argv[1]);
+		printf("Pierwiastek z %d wynosi %d\n", number,
+			 ft_sqrt(number));
+	}
+	else
+	{
+		printf("Podaj jedna liczbe jako arugment.\n");
+	}
+	return 0;
 }
-
 */
--- PiscineC05/ex06/ft_is_prime.c
+++ PiscineC05/ex06/ft_is_prime.c
@@ -6,7 +6,7 @@
 /*   By: dremisze <dremisze@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/06 18:58:01 by dremisze          #+#    #+#             */
-/*   Updated: 2023/12/06 19:28:42 by dremisze         ###   ########.fr       */
+/*   Updated: 2023/12/07 18:52:27 by dremisze         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,12 @@ int	ft_is_prime(int nb)
 {
 	int	i;
 
-	if (nb == 1 || nb == 0)
+	i = 2;
+	if (nb < 2)
 	{
 		return (0);
 	}
-	i = 2;
-	while (i * i <= nb)
+	while (i <= nb / i)
 	{
 		if (nb % i == 0)
 		{
@@ -31,21 +31,22 @@ int	ft_is_prime(int nb)
 }
 /*
 #include <stdio.h>
+#include <stdlib.h>
 
-i#include <stdio.h>
-
-int main()
+int main(int argc, char *argv[])
 {
-    int nb;
-	nb = 3;
-    if (ft_is_prime(nb))
-    {
-        printf("Liczba %d jest liczba pierwsza\n", nb);
-    }
-    else
-    {
-        printf("Liczba %d nie jest liczba pierwsza\n", nb);
-    }
-    return 0;
+	if (argc == 2)
+	{
+		int number = atoi(argv[1]);
+		if (ft_is_prime(number))
+		{
+			printf("Liczba %d jest liczba pierwsza\n", number);
+		}
+		else
+		{
+        	printf("Liczba %d nie jest liczba pierwsza\n", number);
+		}
+	return 0;
+	}
 }
 */
