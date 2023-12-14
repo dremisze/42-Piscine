10cf211290c8dc5cd593f1be87e079662f610e97
Dariusz Remiszewski <dremisze@c3r2s3.42warsaw.pl>
2023-12-14 09:28:47 +0100

    a

---
+++ PiscineC07/ex03/ft_strjoin.c
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dremisze <dremisze@student.42warsaw.pl>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/13 19:05:06 by dremisze          #+#    #+#             */
+/*   Updated: 2023/12/13 21:12:35 by dremisze         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int	str_len(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*create_str(int size, char **strs, char *sep)
+{
+	char		*str;
+	int			len;
+	int			i;
+
+	len = 0;
+	i = 0;
+	while (i < size)
+	{
+		len += str_len(strs[i]);
+		i++;
+	}
+	len += str_len(sep) * (size - 1);
+	if (size <= 0)
+		len = 1;
+	str = (char *)malloc(sizeof(char) * len);
+	if (!str)
+		return (0);
+	return (str);
+}
+
+char	*ft_strjoin(int size, char **strs, char *sep)
+{
+	int			i;
+	int			j;
+	int			k;
+	char		*str;
+
+	str = create_str(size, strs, sep);
+	i = -1;
+	k = 0;
+	while (++i < size)
+	{
+		j = 0;
+		while (strs[i][j])
+		{
+			str[k++] = strs[i][j];
+			j++;
+		}
+		j = 0;
+		while (sep[j] && i != size - 1)
+		{
+			str[k++] = sep[j];
+			j++;
+		}
+	}
+	str[k] = '\0';
+	return (str);
+}
